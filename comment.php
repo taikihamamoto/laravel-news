e9aa93a47f841b65a330f21bb11f6c309a670b73
taikihamamoto <taiki200316@icloud.com>
2021-04-17 03:03:21 +0900

    プルリク

--- comment.php
+++ comment.php
@@ -1,66 +1,105 @@
 <?php
+    $user = 'root';
+    $password = 'root';
+    $db = 'laravel_news';
+    $host = 'localhost';
+    $port = 3306;
+    $link = mysqli_init();
+    $success = mysqli_real_connect(
+        $link,
+        $host,
+        $user,
+        $password,
+        $db,
+        $port
+    );
+    $username = 'laravel_user';
+    $pass = 'C78A]cuWUh_]65k';
+    $dsn = 'mysql:host=localhost;dbname=laravel_news;';
+    try{
+        $dbh = new PDO($dsn,$username,$pass,[
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ]);
+    }catch(Exception $e){
+        echo '接続失敗'.$e->getMessage();
+    }
+
+    //newsSQLの準備
+    $news_sql = 'SELECT * FROM news';
+    //nesSQLの実行
+    $news_stmt = $dbh->query($news_sql);
+    //newsSQLの結果を受け取る
+    $news_result = $news_stmt->fetchall(PDO::FETCH_ASSOC);
+
+    //commentSQLの準備
+    $comments_sql = 'SELECT * FROM comments ORDER BY id DESC';
+    //commentSQLの実行
+    $comments_stmt = $dbh->query($comments_sql);
+    //commentSQLの結果を受け取る
+    $comments_result = $comments_stmt->fetchall(PDO::FETCH_ASSOC);
     define("commentBox","comment.txt");
     $id = $_GET["id"];
     $commentText = "";
-    $message =array();
-    $message_array =array();
     $error_message = array();
     $clean = array();
-    $fp = fopen(commentBox, "a+");
     $titleLimit =50;
-    /*登録番号の定義*/
-    if ( file_exists(commentBox) ) { /*ファイルの存在確認。*/
+    // idの確認
+    if(empty($id)){
+        exit("idがありません");
+    }
+    // 投稿番号の定義
+    foreach($comment_result as $column){ 
     //最後の行にプラス1
-        $lines=file(commentBox);
+        $lines=$comment_result;
         $lastline= $lines[count($lines) - 1];
         $num=explode(",",$lastline);
         $lastnum=$num[0]+1;
-    } else { /*ファイルが無かった場合変数の定義を１とする*/
-    $num = 1;
     }
     if( !empty($_POST["commentSend"])){
+        // commentの確認
         if(empty($_POST["comment"])){
             $error_message[] = "コメントは必須です。";
         }
+        // commentの文字数制限
         elseif(strlen($_POST["comment"]) > $titleLimit){
             $error_message[] = "コメントは5０文字以内です。";
         }
+        // commentの整形
         else{
             $clean["comment"] = htmlspecialchars( $_POST["comment"], ENT_QUOTES);
             $clean["comment"] = preg_replace( '/\\r\\n|\\n|\\r/', '', $clean['comment']);
         }
+        // commentsテーブルに挿入
         if(empty($error_message)){
-            if($fp = fopen(commentBox, "a")){
-                $data = "".$lastnum.",".$id.",".$clean['comment']."\n";
-                fwrite($fp,$data);
-                fclose($fp);
-            }
+            $sql = 'INSERT INTO 
+                        comments (id,news_id,comment) 
+                    VALUES 
+                        (:id,:news_id,:comment)';
+            $comments_stmt = $dbh->prepare($sql);
+            $params = array(':id' => $lastnum,':news_id'=> $id,':comment' => $clean["comment"]);
+            $comments_stmt->execute($params);
+            header('Location: ' . $_SERVER['REQUEST_URI']);
+            //プログラム終了
+            exit;
         }
-    }
-    if($fp = fopen(commentBox, "r")){
-        while ($data = fgets($fp)){
-            $splitData = explode(",",$data);
-            $message = array(
-                "commentnum" => $splitData[0],
-                "id" => $splitData[1],
-                "comment" => $splitData[2]
-            );
-            array_unshift($message_array, $message);
-        }
-        fclose($fp);
     }
     if (isset($_POST['deleteSend'])) {
-
+        // commentsテーブルから削除
         $delete = $_POST['delete'];
-        $delCon = file(commentBox);
-        for ($j = 0; $j < count($delCon) ; $j++){ 
-        $delData = explode(",", $delCon[$j]);
-        
-        if ($delData[0] == $delete) { 
-        array_splice($delCon, $j, 1);
-        file_put_contents(commentBox, implode("", $delCon));
+        foreach($comments_result as $column){
+            if($delete == $column["id"]){
+                $sql = 'DELETE FROM 
+                            comments 
+                        WHERE
+                            id=:id';
+            $comments_stmt = $dbh->prepare($sql);
+            $params = array(':id' => $column["id"]);
+            $comments_stmt->execute($params);
+            }
         }
-        }
+        header('Location: ' . $_SERVER['REQUEST_URI']);
+    //プログラム終了
+    exit;
     }
 ?>  
 <!DOCTYPE html>
@@ -69,12 +108,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="home.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Laravel News</title>
 </head>
 <body>
-    <nav>
-        <a href="home.php">Laravel News</a>
+    <nav class="main-header">
+        <div class="nav-bar">    
+            <a href="home.php" class="nav-link">Laravel News</a>
+        </div>
     </nav>
     <?php if(!empty($error_message)): ?>
         <ul class="error_message">
@@ -83,31 +124,37 @@
                 <?php endforeach;?>
         </ul>
     <?php endif;?>
-    <section>
-        <h3><?php echo $_GET["title"];?></h3>
-        <p><?php echo $_GET["body"];?></p>
+    <section class="post-detail">
+        <?php foreach($news_result as $column): ?>
+        <?php if($id == $column["id"]): ?>
+        <h3 class="post-title"><?php echo $column["title"];?></h3>
+        <p class="post-body"><?php echo $column["body"];?></p>
+        <?php endif; ?>
+        <?php endforeach; ?>
     </section>
     <hr>
-    <section>
+    <section class="comments">
+    <div class="form-comment">
     <form method="POST" action="">
-        <textarea name="comment"></textarea>
-        <input type="submit" name="commentSend" value="コメントを書く"></form>
-    </form>
-    </section>
-    <section>
-    <?php foreach($message_array as $value ): ?>
-        <form method="POST" action="">
-            <?php
-            if(!empty($message_array)): ?>
-            <?php if($id == $value["id"]): ?>
-            <div>
-            <div><?php echo $value["comment"]; ?></div>
-            <input type="hidden" name="delete" value="<?php echo $value["commentnum"];?>">
-            <input type="submit" name="deleteSend" value="コメントを消す">
+        <div class="input-body">
+            <textarea name="comment" class="post-it post-it-red"></textarea>
+            <input class="btn-submit" type="submit" name="commentSend" value="コメントを書く">
         </div>
+    </form>
+    </div>
+    <?php foreach($comments_result as $column): ?>
+            <?php
+            if(!empty($comments_result)): ?>
+            <?php if($id == $column["news_id"]): ?>
+                <div class="commentContent">
+                <?php echo $column["comment"]; ?>
+                <form method="POST" action="">
+                <input type="hidden" name="delete" value="<?php echo $column["id"];?>">
+                <input class="deleteComment" type="submit" name="deleteSend" value="コメントを消す">
         <?php endif; ?>
         <?php endif; ?>
         </form>
+        </div>
         <?php endforeach; ?>
     </section>
 </body>
